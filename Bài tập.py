faaba9d738a4424e539a15ae70386b3b3d7875aa
khoa hoc du lieu 17A2 <lhchuyen.dhkl17a2hn@sv.uneti.edu.vn>
2023-10-30 17:07:38 +0700

    Thêm file baitap

---
+++ Bài tập.py
@@ -0,0 +1,14 @@
+#1.1
+print("** ** ******    **      **     ** ****" )
+print("** ** **        **      **     **   **")
+print("***** ******    **      **     **   **")
+print("** ** **        **      **     **   **")
+print("** ** ** ****   ******  ******  ******")
+#1.3
+ten_mon_hang = "Sữa hộp Vina milk"
+so_luong = 5
+don_gia = 25000
+so_tien_phai_tra = so_luong * don_gia
+print("Tên hàng:", ten_mon_hang)
+print("Số lượng:", so_luong)
+print("Tiền phải trả:", so_tien_phai_tra)
